3cf0fd4a2c8890ffb6d0dc3783a7cdd9f60d8bde
Maruful <tonmoykhan46@yahoo.com>
2021-02-27 13:03:39 +0600

    cart added

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+    console.log(cart)
+    //const total = cart.reduce((total, prd) => total + prd.price , 0)
+    let total = 0;
+    for (let i =0; i < cart.length; i++) {
+        const product = cart[i];
+        total = total + product.price;
+        console.log ("price: "+product.price)
+    }
+    const vat = Math.round(total / 10);
+    let shipping = 0;
+    if (total > 0) {
+        shipping = 6;
+    }
+    else if (total > 30) {
+        shipping = 4;
+    }
+    else if (total > 50) {
+        shipping = 1;
+    }
+    return (
+        <div>
+            <h3>Order summary</h3>
+            <p>Items ordered: {cart.length}</p>
+            <p>Product price: {Math.round(total)}</p>
+            <p>Shipping cost: {shipping}</p>
+            <p>Total Vat: {vat}</p>
+            <p>Total price: {Math.round(total + vat + shipping)} </p>
+        </div>
+    );
+};
+
+export default Cart;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
 import Product from '../../product/Product';
+import Cart from '../Cart/Cart';
 import './shop.css';
 const Shop = () => {
   const data = (fakeData.slice(0, 10));
+  const [cart, setCart] = useState([])
   const [products, setProducts] = useState(data)
-  const handleAddProduct = (product) => {console.log (product)};
+  const handleAddProduct = (product) => {
+      console.log (product)
+      const newCart = [...cart, product];
+      setCart(newCart);
+
+    };
     return (
         <div className="shop-container">
             <div className="product-container">
@@ -14,7 +21,7 @@ const Shop = () => {
             }
             </div>
             <div className="cart-container">
-                <h1>This is cart</h1>
+                <Cart cart={cart}></Cart>
             </div>
       
         </div>
